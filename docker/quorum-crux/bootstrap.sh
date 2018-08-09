0921494013f43a070b648b61d0a8ef33ac6eac55
Puneetha Karamsetty <puneetha17@gmail.com>
2018-08-09 13:07:48 +0100

    Add Docker files (#24)
    * Update the gRPC address and add logs when connected to other nodes
    
    * Add docker files to create crux & quorum-crux containers
    
    * Update Readme to include references to docker build

---
+++ docker/quorum-crux/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu -o pipefail
+
+# make/install quorum
+git clone https://github.com/ConsenSys/quorum.git
+pushd quorum >/dev/null
+git checkout tags/v2.0.2-grpc
+make all
+cp build/bin/geth /usr/local/bin
+cp build/bin/bootnode /usr/local/bin
+rm -r build
+popd >/dev/null
+
+# make/install crux
+git clone https://github.com/blk-io/crux.git
+cd crux
+git checkout tags/v1.0.0
+make setup && make
+cp bin/crux /usr/local/bin
+rm -r bin

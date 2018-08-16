74e468791eb2df570d8cbd370e198b2ed338bdff
drandreaskrueger <andreas.krueger@electron.org.uk>
2018-08-16 12:07:51 +0100

    higher gasLimit, larger txpool, blockperiod 1 seconds

--- docker/quorum-crux/istanbul-start.sh
+++ docker/quorum-crux/istanbul-start.sh
@@ -8,7 +8,7 @@ echo "[*] Starting Crux nodes"
 
 echo "[*] Starting Ethereum nodes"
 set -v
-ARGS="--syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul "
+ARGS="--txpool.globalslots 20000 --txpool.globalqueue 20000 --istanbul.blockperiod 1 --syncmode full --mine --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul "
 PRIVATE_CONFIG=qdata/c/tm.ipc nohup geth --datadir qdata/dd $ARGS --rpcport $GETH_RPC_PORT --port $GETH_PORT --nodekeyhex $NODE_KEY --unlock 0 --password passwords.txt --verbosity=6 2>>qdata/logs/node.log &
 set +v
 
